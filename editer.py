018f9e7f939beb1ddd3a523af804a74c48742461
18377016 <3089449921@qq.com>
2020-03-22 10:49:53 +0800

    editer.py

--- editer.py
+++ editer.py
@@ -4,3 +4,4 @@ number=input()                                  #学号
 print("NAME:"+name)                             #输出姓名
 print("ID:"+number)                             #输出学号
 print("Welcome to BUAA Online Judge!")          #输出欢迎词
+###
