dd68c9ba780e4c8ab21da0cbdcb845429c070b59
18377016 <3089449921@qq.com>
2020-03-23 21:55:07 +0800

    first update

---
+++ text_editor.py
@@ -0,0 +1,7 @@
+##text_editor
+from tkinter.messagebox import showinfo
+from tkinter import*
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+import os
+import webbrowser
