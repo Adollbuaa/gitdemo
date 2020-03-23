47626c1ac010958d33a8b2237c406ee27853296c
18377016 <3089449921@qq.com>
2020-03-23 22:00:35 +0800

    second update

--- text_editor.py
+++ text_editor.py
@@ -5,3 +5,13 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import os
 import webbrowser
+top=Tk()
+top.title('文本编辑器')
+top.geometry('450x200')
+
+label1=Label(top,text='文本:')
+label2=Label(top,text='打开文件:')
+whitelabel1=Label(top,text='')
+whitelabel2=Label(top,text='')
+whitelabel3=Label(top,text='温馨提示:')
+whitelabel4=Label(top,text='‘文本’与‘打开文件’同时编辑时，默认‘打开文件’')
